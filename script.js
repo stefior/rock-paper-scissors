c93dabab24c5304c7f24b5aa215750f718a85a13
Stefior <stephencfiorenza@gmail.com>
2022-09-08 13:32:06 -0400

    Add keeping score functionality to results

--- script.js
+++ script.js
@@ -16,24 +16,30 @@ function playRound(playerSelection, computerSelection) {
     if (computerSelection === 'rock') {
       return 'tie';
     } else if (computerSelection === 'paper') {
+      computerScore++;
       return 'you lose. paper beats rock';
     } else if (computerSelection === 'scissors') {
+      playerScore++;
       return 'you win. rock beats scissors';
     }
   }
   if (playerSelection === 'paper') {
     if (computerSelection === 'rock') {
+      playerScore++;
       return 'you win. paper beats rock';
     } else if (computerSelection === 'paper') {
       return 'tie';
     } else if (computerSelection === 'scissors') {
+      computerScore++;
       return 'you lose. scissors beats paper';
     }
   }
   if (playerSelection === 'scissors') {
     if (computerSelection === 'rock') {
+      computerScore++;
       return 'you lose. rock beats scissors';
     } else if (computerSelection === 'paper') {
+      playerScore++;
       return 'you win. scissors beats paper';
     } else if (computerSelection === 'scissors') {
       return 'tie';
@@ -43,4 +49,9 @@ function playRound(playerSelection, computerSelection) {
 
 const buttons = document.querySelectorAll('button');
 const results = document.querySelector('#results');
-buttons.forEach((button) => button.addEventListener('click', () => results.textContent = playRound(button.textContent, getComputerChoice())));
+results.style.cssText = 'white-space: pre;';
+let playerScore = 0;
+let computerScore = 0;
+buttons.forEach((button) => button.addEventListener('click', () => {results.innerHTML = `${playRound(button.textContent, getComputerChoice())}\r\n
+your score: ${playerScore}\r\n
+computer's score: ${computerScore}`}));
